955a9c69159b73299270e7d9d6018cbaacdb1dba
Laksh Tank <tanklaksh.2007@gmail.com>
2026-08-27 12:15:00 +0530

    fix: polyfill DOMMatrix for pdf-parse v2 in serverless environments

--- server/services/ocr.service.js
+++ server/services/ocr.service.js
@@ -7,7 +7,55 @@ try {
 } catch (e) {
   sharp = null;
 }
-const { PDFParse } = require('pdf-parse');
+// Polyfill DOMMatrix and DOMPoint for Node.js serverless runtimes required by pdf-parse v2
+if (typeof global.DOMMatrix === 'undefined') {
+  global.DOMMatrix = class DOMMatrix {
+    constructor() {
+      this.a = 1;
+      this.b = 0;
+      this.c = 0;
+      this.d = 1;
+      this.e = 0;
+      this.f = 0;
+    }
+    multiply() {
+      return this;
+    }
+    translate() {
+      return this;
+    }
+    scale() {
+      return this;
+    }
+    rotate() {
+      return this;
+    }
+    inverse() {
+      return this;
+    }
+    transformPoint(p) {
+      return p;
+    }
+  };
+}
+if (typeof global.DOMPoint === 'undefined') {
+  global.DOMPoint = class DOMPoint {
+    constructor(x = 0, y = 0, z = 0, w = 1) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.w = w;
+    }
+  };
+}
+
+let PDFParse;
+try {
+  const pdfParsePkg = require('pdf-parse');
+  PDFParse = pdfParsePkg.PDFParse || pdfParsePkg;
+} catch (e) {
+  PDFParse = null;
+}
 const { PDFDocument, PDFName, PDFRawStream, PDFStream } = require('pdf-lib');
 
 /**
