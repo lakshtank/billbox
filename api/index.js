c9186cec194f8b3be5d55f4f9f1e9688ab9404b9
Laksh Tank <tanklaksh.2007@gmail.com>
2026-08-27 12:12:40 +0530

    debug: add serverless diagnostic wrapper to expose exact error

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
-const app = require('../server/server');
+let app;
+let loadError = null;
 
-module.exports = app;
+try {
+  app = require('../server/server');
+} catch (err) {
+  loadError = err;
+  console.error('FAILED TO LOAD SERVER:', err);
+}
+
+module.exports = (req, res) => {
+  if (loadError) {
+    return res.status(500).json({
+      success: false,
+      message: 'Server failed to load: ' + loadError.message,
+      stack: loadError.stack,
+    });
+  }
+  return app(req, res);
+};
